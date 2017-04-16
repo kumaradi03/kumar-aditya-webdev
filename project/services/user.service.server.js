537eae982a5e3dfc0a7a314833c6e2be35321148
Aditya Kumar <aditya@Adityas-MacBook-Pro.local>
2017-04-16 10:30:41 -0400

    Project home page

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -36,11 +36,18 @@ module.exports = function (app,model){
     var movieUserModel = model.movieUserModel;
     var movieModel = model.movieModel;
 
+    // var facebookConfig = {
+    //     clientID     : "280520602398806",
+    //     clientSecret : "245a429b9b31d3097936b1e954d82618",
+    //     callbackURL  : "http://127.0.0.1:3000/auth/facebook/callback"
+    // };
+
     var facebookConfig = {
-        clientID     : "280520602398806",
-        clientSecret : "245a429b9b31d3097936b1e954d82618",
-        callbackURL  : "http://127.0.0.1:3000/auth/facebook/callback"
+        clientID     : process.env.FACEBOOK_CLIENT_ID,
+        clientSecret : process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL  : process.env.FACEBOOK_CALLBACK_URL
     };
+
 
     passport.use('local',new LocalStrategy(localStrategy));
     passport.use('facebook',new FacebookStrategy(facebookConfig, facebookLogin));

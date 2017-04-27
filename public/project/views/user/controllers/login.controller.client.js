c8927f4a4243a36d624f4309998a6e1833e908e0
Aditya Kumar <aditya@Adityas-MacBook-Pro.local>
2017-04-27 00:02:20 -0400

    Form Validation

--- public/project/views/user/controllers/login.controller.client.js
+++ public/project/views/user/controllers/login.controller.client.js
@@ -6,21 +6,27 @@
         .module("Movies&More")
         .controller("LoginController", LoginController);
 
-    function LoginController(UserService, $location) {
+    function LoginController(UserService, $location,$scope) {
         var vm = this;
         vm.login = login;
 
         function login(user) {
-            UserService
-                .login(user)
-                .then(function (loginUser) {
-                    if(loginUser)
-                        $location.url('/profile/');
-                    else
-                        vm.error = 'User not found.';
-                },(function (err) {
-                   vm.error = "Username and Password doesn't match."
-                }));
+            if($scope.loginNew.$valid){
+                UserService
+                    .login(user)
+                    .then(function (loginUser) {
+                        if(loginUser)
+                            $location.url('/profile/');
+                        else
+                            vm.error = 'User not found.';
+                    },(function (err) {
+                        vm.error = "Username and Password doesn't match."
+                    }));
+            }
+            else{
+                $scope.loginNew.submitted = true;
+                vm.error ="Form Incomplete";
+            }
         }
     }
 })();
